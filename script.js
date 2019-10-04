d80a42adf653fe3f292c23ebe1b0e6596fae54e1
Eurvin Morman <eurvin@gmail.com>
2019-10-04 17:17:46 +0200

    added letters array

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
-document.addEventListener('DOMContentLoaded', function (event) {
-        const words = ['programmeren', 'javascript', 'slack', 'tutorials', 'github', 'samenwerken',
-        'studeren', 'browser', 'terminal'];
+document.addEventListener('DOMContentLoaded', function(event) {
+  const words = ['programmeren', 'javascript', 'slack', 'tutorials', 'github', 'samenwerken',
+    'studeren', 'browser', 'terminal'
+  ];
 
-    const word = words[Math.random() * words.length | 0];
+  const word = words[Math.random() * words.length | 0];
 
-    console.log(word);
+  console.log(word);
+  const letters = Array.from(word);
+  console.log(letters);
+  console.log(letters.length);
+  // let letter = document.querySelector('.answer h4:nth-child(2)');
+  //
+  // letters.forEach(function(letter) {
+  //
+  //   letter.innerHTML = "<span>1</span>";
+  //   console.log(letter);
+  // });
 
-    let lives = 5;
-    console.log(lives);
-    let liveElement = document.getElementById("lives");
-    console.log(liveElement);
-    liveElement.innerHTML = "you have " + lives + " lives remaining";
-    console.log(liveElement);
 
-    const guess= document.getElementsByClassName("guess")
-    guess.addEventListener('click', function () {
-        if (/*letter zit niet in woord*/) {
-            lives--;
-        } else if (lives === 0) {
-            alert("Game Over!");
-        } else {
-            //letter verschijnt op scherm
-        };
-    });
+  let lives = 5;
+  console.log(lives);
+  let liveElement = document.getElementById("lives");
+  console.log(liveElement);
+  liveElement.innerHTML = "you have " + lives + " lives remaining";
+  console.log(liveElement);
+
+  const guess = document.getElementsByClassName("guess")
+  guess.addEventListener('click', function() {
+    if(true) {
+      lives--;
+      console.log();
+    } else if(lives === 0) {
+      alert("Game Over!");
+    } else {
+      //letter verschijnt op scherm
+    };
+  });
 
 
 
