266eee924ea12c032a2c61604adeb4e882ac652c
Heather Hoekstra <heatherhoekstra@upcmail.nl>
2019-10-04 14:08:42 +0200

    aangepaste bestandsnaam

---
+++ script.js
@@ -0,0 +1,6 @@
+const words= ['programmeren', 'javascript', 'slack', 'tutorials', 'github', 'samenwerken',
+'studeren', 'browser', 'terminal'];
+
+const word= words[Math.random() * words.length | 0];
+
+console.log(word);
