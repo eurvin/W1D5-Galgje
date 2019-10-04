d6743c49e4a1b64f2386ecada96c161dce03e5d8
Heather Hoekstra <heatherhoekstra@upcmail.nl>
2019-10-04 16:39:37 +0200

    het aftellen van levens, en een bericht als game over

--- script.js
+++ script.js
@@ -1,43 +1,31 @@
-document.addEventListener('DOMContentLoaded', function(event) {
-    //the event occurred
-    const words= ['programmeren', 'javascript', 'slack', 'tutorials', 'github', 'samenwerken',
-    'studeren', 'browser', 'terminal'];
+document.addEventListener('DOMContentLoaded', function (event) {
+        const words = ['programmeren', 'javascript', 'slack', 'tutorials', 'github', 'samenwerken',
+        'studeren', 'browser', 'terminal'];
 
-    const word= words[Math.random() * words.length | 0];
+    const word = words[Math.random() * words.length | 0];
 
     console.log(word);
-
-    /*5 levens. Loop. elke keer als er iets gebeurt, gaat er 1 af. (i--?)
-    for loop. var i. Elke keer dat het fout is 1 eraf. (of if else)
-    alert gebruiken als game over.
-
-
-    */
-
-
-
-
-
-
-
 
     let lives = 5;
     console.log(lives);
     let liveElement = document.getElementById("lives");
     console.log(liveElement);
-    liveElement.innerHTML= "you have " + lives + " lives remaining";
+    liveElement.innerHTML = "you have " + lives + " lives remaining";
     console.log(liveElement);
-  })
+
+    document.getElementsByClassName("guess")
+    guess.addEventListener('click', function () {
+        if (/*letter zit niet in woord*/) {
+            lives--;
+        } else if (lives === 0) {
+            alert("Game Over!");
+        } else {
+            //letter verschijnt op scherm
+        };
+    });
 
 
 
-/*function (activeren als geklikt wordt, in zetten dat startleven 5 is){
-if (guess = goed){
 
-}
-else {
-    lives--
-}
- }
-*/
- //en dan de levens in het bestand schrijven. #lives
+
+});
