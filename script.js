ff86f53b6a5851b2cada3c3469b7ebea63e02ee9
Eurvin Morman <eurvin@gmail.com>
2019-10-06 11:14:13 +0200

    minor changes

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-document.addEventListener('DOMContentLoaded', function (event) {
+document.addEventListener('DOMContentLoaded', function(event) {
 
   // word & letters
   const words = ['programmeren', 'javascript', 'slack', 'tutorials', 'github',
-  'samenwerken', 'studeren', 'browser', 'terminal'];
+    'samenwerken', 'studeren', 'browser', 'terminal'
+  ];
 
   // create a new array of letters with a random word from words array
   const wordArray = Array.from(words[Math.random() * words.length | 0]);
@@ -15,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
   let liveElement = document.getElementById("lives");
 
   // array to play game with starting with underscores
-  const tryArray = wordArray.map(x => x = '_');
+  const tryArray = wordArray.map(letter => letter = '_');
 
   // Display tryArray in 2nd <h4> element
   const letterElement = function(tryArray) {
@@ -27,11 +28,11 @@ document.addEventListener('DOMContentLoaded', function (event) {
 
   // Update liveElement
   const updateLives = (lives) => {
-    if(lives <= 0) {
+    if (lives <= 0) {
       liveElement.innerHTML = `Game over! You have no lives remaining.
       The right word was ${wordArray.join('')}!
       Press the restart button to play again!`;
-    } else if(lives == 'win') {
+    } else if (lives == 'win') {
       liveElement.innerHTML = `You have won the game! Good job!
       Press the restart button to play again!`;
     } else {
@@ -44,14 +45,14 @@ document.addEventListener('DOMContentLoaded', function (event) {
   updateLives(lives);
 
   // Restart the Game
-  const restartButton = document.getElementById("restart").addEventListener('click', function(){
+  const restartButton = document.getElementById("restart").addEventListener('click', function() {
     location.reload();
   });
 
   // Get letter from input
-  const guessButton = document.getElementById("guess").addEventListener('click', function () {
+  const guessButton = document.getElementById("guess").addEventListener('click', function() {
     let letter = document.getElementById("try").value;
-    livesLeft(letter)
+    livesLeft(letter);
   });
 
   // Check lives
@@ -77,12 +78,12 @@ document.addEventListener('DOMContentLoaded', function (event) {
 
   // check if letter is correct
   const checkLetter = function(letter) {
-    if(wordArray.includes(letter) == false) {
+    if (wordArray.includes(letter) == false) {
 
       // subtract 1 lives + update letters on screen
       lives--;
-      if(lettersArray.includes(letter) == false){
-        
+      if (lettersArray.includes(letter) == false) {
+
         lettersArray.push(letter);
         updateLives(lives);
       }
@@ -91,7 +92,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
       // change letter in tryArray if found in wordArray
       let i = 0;
       tryArray.map(function(x) {
-        if(wordArray[i] == letter) {
+        if (wordArray[i] == letter) {
           tryArray[i] = letter;
           i++;
         } else {
@@ -105,9 +106,9 @@ document.addEventListener('DOMContentLoaded', function (event) {
 
   // check if game is won and display message + alert
   const checkWin = function(tryArray) {
-      if (wordArray.join('') == tryArray.join('')) {
-        updateLives('win');
-        alert('We have a winner!');
+    if (wordArray.join('') == tryArray.join('')) {
+      updateLives('win');
+      alert('We have a winner!');
     }
   }
 
